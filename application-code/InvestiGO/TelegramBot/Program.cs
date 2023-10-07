af056650f203e141c3d51c710b4b5e2ed124e152
felipesuarez <suarez.a.felipe@gmail.com>
2023-10-07 19:33:47 +0400

    Add logic to read messages from a specific group channel. Store messages in EF Core InMemoryDB.

--- application-code/InvestiGO/TelegramBot/Program.cs
+++ application-code/InvestiGO/TelegramBot/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using TelegramBot;
+using TelegramBot.Data;
 
-Console.WriteLine("Hello, World!");
+using var dbContext = new AppDbContext();
+var bot = new Bot("6571967465:AAFXuqGMd6b3a49PZg_PrpBF4i0-0qtgvfI", dbContext, -1001839414047);
+bot.Start();
+
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
